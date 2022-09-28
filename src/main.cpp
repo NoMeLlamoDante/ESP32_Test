7e77df8ec48f10f7cbd5b311324ca7ff54d9b64e
dante <eo.zm24@outlook.com>
2022-09-28 15:42:43 -0500

    Prueba Servidor MQTT

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,46 @@
+// Libraries 
 #include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
 
-void setup() {
-  // put your setup code here, to run once:
+//Data
+const char* ssid = "IZZI-0CC8";
+const char* password = "704FB8270CC8";
+const char* mqttServer = "broker.emqx.io";
+const int mqttPort = 1883;
+const char* mqttUser = "";
+const char* mqttPassword = "";
+
+//Data Lib
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+
+void setup()
+{ Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  Serial.println("...................................");
+
+  Serial.print("Intentando Conectar Wifi.");
+  while (WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".") ;
+  }
+
+  Serial.println("Conectado a Wifi");
+  client.setServer(mqttServer, mqttPort);
+  while (!client.connected()){
+    Serial.println("Conectando al broker MQTT...");
+    if (client.connect("mqttx_54d756f3", mqttUser, mqttPassword ))
+      Serial.println("Conectado");
+    else{
+      Serial.print("Fallo con estado:");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
 }
