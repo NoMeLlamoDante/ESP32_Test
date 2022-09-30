f9fb125d39b4fedaf9b4c44dd7ed48d8322c1522
dante <eo.zm24@outlook.com>
2022-09-30 17:14:57 -0500

    Get Time Via Wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,32 @@
 // Libraries 
 #include <Arduino.h>
 #include <WiFi.h>
-#include <PubSubClient.h>
+#include <PubSubClient.h> //MQTT
+#include <NTPClient.h>  //Time
+#include <WiFiUdp.h>
 
 //Data
+//Wifi Data
 const char* ssid = "IZZI-0CC8";
 const char* password = "704FB8270CC8";
+//Broker Data
 const char* mqttServer = "broker.emqx.io";
 const int mqttPort = 1883;
 const char* mqttUser = "";
 const char* mqttPassword = "";
+//Time Data
+const long utcOffsetInSeconds = -18000;
+char daysOfTheWeek[7][10] = {"Domingo","Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"};
 
 //Data Lib
 WiFiClient espClient;
 PubSubClient client(espClient);
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "mx.pool.ntp.org",utcOffsetInSeconds);
 
 
-
-void setup()
-{ Serial.begin(9600);
+void setup(){
+  Serial.begin(9600);
   //Conectar Wifi
   WiFi.begin(ssid, password);
   Serial.println("...................................");
@@ -31,7 +39,7 @@ void setup()
 
   Serial.println("Conectado a Wifi");
   client.setServer(mqttServer, mqttPort);
-
+  timeClient.begin();
   //Conectar Broker
   while (!client.connected()){
     Serial.println("Conectando al broker MQTT...");
@@ -46,9 +54,12 @@ void setup()
 }
 
 void loop() {
+  timeClient.update();
   client.loop();
+
+  Serial.println(timeClient.getFormattedTime());
     
 
   client.publish("dante_prueba/esp_32",String(millis()).c_str());
-  delay(500);
+  delay(1000);
 }
