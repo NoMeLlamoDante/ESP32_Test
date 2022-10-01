b3e8c18b29bb06bb0d35a3c50eaf92d6e80c7403
dante <eo.zm24@outlook.com>
2022-09-30 20:50:21 -0500

    Prueba grabando texto desde micro SD

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,10 @@
 #include <WiFi.h>
 #include <PubSubClient.h> //MQTT
 #include <NTPClient.h>  //Time
-#include <WiFiUdp.h>
+#include <WiFiUdp.h>  //Wifi - Time
+#include <FS.h>
+#include <SD.h>
+#include <SPI.h>
 
 //Data
 //Wifi Data
@@ -16,7 +19,6 @@ const char* mqttUser = "";
 const char* mqttPassword = "";
 //Time Data
 const long utcOffsetInSeconds = -18000;
-char daysOfTheWeek[7][10] = {"Domingo","Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"};
 
 //Data Lib
 WiFiClient espClient;
@@ -29,15 +31,13 @@ void setup(){
   Serial.begin(9600);
   //Conectar Wifi
   WiFi.begin(ssid, password);
-  Serial.println("...................................");
-
   Serial.print("Intentando Conectar Wifi.");
   while (WiFi.status() != WL_CONNECTED){
     delay(500);
     Serial.print(".") ;
   }
-
   Serial.println("Conectado a Wifi");
+  //Time
   client.setServer(mqttServer, mqttPort);
   timeClient.begin();
   //Conectar Broker
@@ -51,15 +51,53 @@ void setup(){
       delay(2000);
     }
   }
+  //SD Mount
+  Serial.println("Verificando Micro SD");
+  if(!SD.begin(5)){
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
+  
+  if(cardType == CARD_NONE){
+    Serial.println("No SD card attached");
+    return;
+  }
+
+  Serial.print("SD Card Type: ");
+  if(cardType == CARD_MMC){
+    Serial.println("MMC");
+  } else if(cardType == CARD_SD){
+    Serial.println("SDSC");
+  } else if(cardType == CARD_SDHC){
+    Serial.println("SDHC");
+  } else {
+    Serial.println("UNKNOWN");
+  }
+
+  uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+  Serial.printf("SD Card Size: %lluMB\n", cardSize);
+
 }
 
 void loop() {
-  timeClient.update();
+  //MQTT
   client.loop();
-
-  Serial.println(timeClient.getFormattedTime());
-    
-
   client.publish("dante_prueba/esp_32",String(millis()).c_str());
+  //Time
+  timeClient.update();
+  Serial.println(timeClient.getFormattedTime());
   delay(1000);
+  //SD
+  File file = SD.open("/datalog.txt", FILE_WRITE);
+  if(!file){
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if(file.println(timeClient.getDay()+" ,Hora: "+timeClient.getFormattedTime())){
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+  file.close();
 }
